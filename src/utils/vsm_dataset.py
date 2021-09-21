626115e4489a0969f4ac3fa8fe6314a54a0f0705
StevRamos <stevhr@gmail.com>
2021-09-21 02:01:41 -0500

    vsm dataset done, unit tests done

--- src/utils/vsm_dataset.py
+++ src/utils/vsm_dataset.py
@@ -6,7 +6,7 @@ class VSMDataset(torch.utils.data.Dataset):
         Datasets: TVSum, Summe, VSUMM, CoSum, Visiocity
     """
 
-    def __init__(self, hdfs_path, split=None):
+    def __init__(self, hdfs_path, split=None, transform=None):
         """
         Args:
            hdfs_path (string): path of the hdfs processed data
@@ -15,14 +15,20 @@ class VSMDataset(torch.utils.data.Dataset):
         videos_info = h5py.File(hdfs_path)
         self.labels = {}
         self.data = {}
-
+        self.transform = transform
+        
         for it, video in enumerate(list(videos_info)):
             self.labels[it] = dict((key, videos_info[video][key][...])for key in list(videos_info[video]) if key in ('gtscore', 'gtsummary', 'user_summary') )
             self.data[it] = dict((key, videos_info[video][key][...])for key in list(videos_info[video]) if key not in ('gtscore', 'gtsummary', 'user_summary') )   
-
+            
+            if "video_name" in self.data[it].keys():
+                self.data[it]["video_name"] = str(self.data[it]["video_name"]) 
+            
         if split:
             #TO-DO
             pass
+        
+
 
     def __len__(self):
         return len(self.data)
@@ -30,7 +36,7 @@ class VSMDataset(torch.utils.data.Dataset):
     def __getitem__(self, index):
         X = self.data[index]
         y = self.labels[index]
-
+        
         return X, y
 
 def show_sample(idx, hdfs_path):
