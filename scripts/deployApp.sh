276d13057c9767261ce7d7fcf9b41c0f1b0a5db1
StevRamos <stevhr@gmail.com>
2021-11-10 14:54:37 -0500

    xd

--- scripts/deployApp.sh
+++ scripts/deployApp.sh
@@ -1,4 +1,11 @@
 #execute this from the project home
+echo 'Creating the environment ...'
+conda create -y -n vsm python=3.8.11
+
+echo 'Installing packages ...'
+conda activate vsm
+pip install -r requirements.txt
+
 echo 'Downloading weights of the model ...'
 mkdir weights_model
 cd weights_model
@@ -8,12 +15,18 @@ gshell download tvsum_random_non_overlap_0.6271.tar.pth
 gshell download flow_imagenet.pt
 gshell download r3d101_KM_200ep.pth
 cd ..
+
 echo 'Downloading api keys ...'
 gshell cd --with-id 1P2Db3pAqADpLK5Jn9aDQoHaBGGlx0hrU #download api keys gdrive
 gshell download client_secret_vsm_api.json
 gshell download token_drive_v3.pickle
 gshell download dummy.pk
-echo 'Building docker image ...'
-sudo docker build -f Dockerfile -t vsm .
-echo 'Run the docker container'
-sudo docker run -d -p 8000:8000 -ti vsm /bin/bash -c "cd src/ && source activate vsm && uvicorn app:app --reload --host 0.0.0.0"
+
+echo 'Deploying the app ...'
+nohup uvicorn app:app --reload --host 0.0.0.0 &
+
+#
+#echo 'Building docker image ...'
+#sudo docker build -f Dockerfile -t vsm .
+#echo 'Run the docker container'
+#sudo docker run -d -p 8000:8000 -ti vsm /bin/bash -c "cd src/ && source activate vsm && uvicorn app:app --reload --host 0.0.0.0"
